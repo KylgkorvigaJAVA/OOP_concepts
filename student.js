25da2675c5018c525d46a2d284525cdecd9eaa82
Kaspar <kaspar.viitkar@voco.ee>
2024-11-15 15:15:24 +0200

    Ylesanne 1 lahendatud

---
+++ student.js
@@ -0,0 +1,30 @@
+
+
+class Student {
+    constructor (name) {
+        this.name = name
+        this.finished = false
+    }
+}
+
+/* const names = ['John', 'Mary', 'Denis']
+const students = []
+
+names.forEach(name => {
+    students.push(new Student(name))
+})
+
+console.log(students) */
+
+/* const student1 = new Student('John')
+console.log(student1)
+console.log(student1.name)
+const student2 = new Student('Mary')
+student2.finished = true
+console.log(student2)
+console.log(student2.name)
+console.log(student2.finished) */
+
+const student = new Student("John")
+console.log(student.name)
+console.log(student.finished)
